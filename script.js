b970c037a829675e86fd4664e161e5cbffd6f629
felipelyra3 <felipelyra3@gmail.com>
2022-06-19 05:19:35 -0300

    2022-06-19_CartasFicandoViradasQuandoSaoIguaisEDesvirandoQuandoDiferentes

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ numeroDeCartas = Number(numeroDeCartas);
 let viradas = 0;
 let tentativas = 0;
 const arrayDeSorteio = [];
+let primeiraCarta = null;
+let segundaCarta = null;
 
 
 /* while (numeroDeCartas < 4 || numeroDeCartas > 14 || (numeroDeCartas % 2 !== 0)) {
@@ -33,6 +35,7 @@ let parrots = document.querySelector('.parrots');
 //Gera as cartas embaralhadas no HTML
 for (let i = 0; i < numeroDeCartas; i++) {
     parrots.innerHTML += `<div class = "frontCard card0${i + 1}" onclick="toggleFlip(this)"><img src="./Assets/0${arrayDeSorteio[i]}.gif"></div>`;
+    /* parrots.innerHTML += `<div class = "frontCard card" onclick="toggleFlip(this)"><img src="./Assets/0${arrayDeSorteio[i]}.gif"></div>`; */
 }
 
 function toggleFlip(element) {
@@ -41,15 +44,29 @@ function toggleFlip(element) {
     let contador = document.querySelector('.contador');
     contador.innerHTML = `<p>${tentativas}</p>`;
 
-    let frontCard = document.querySelector('.frontCard');    
-    element.classList.add("backCard");  
-    
+    //Vira as cartas clicadas
+    let frontCard = document.querySelector('.frontCard');
+    element.classList.add("backCard");
+
+    if (primeiraCarta === null) {
+        primeiraCarta = element;
+    } else if (segundaCarta === null) {
+        segundaCarta = element;
+        verificaViradas(element);
+    }
+
 }
 
 function verificaViradas(element) {
-   /*  let queryselector = `document.querySelector('.card${i}')`;
-    for (let i = 0; i < numeroDeCartas; i++) {
-        let card = queryselector;
-        console.log(card);
-    } */
+    if (primeiraCarta.innerHTML == segundaCarta.innerHTML) {
+        console.log('São iguais');
+        primeiraCarta = null;
+        segundaCarta = null;
+    } else {
+        setTimeout(function () { primeiraCarta.classList.remove("backCard"); primeiraCarta = null; }, 1000);
+        setTimeout(function () { segundaCarta.classList.remove("backCard"); segundaCarta = null; }, 1000);
+    }
+
+
+
 }
